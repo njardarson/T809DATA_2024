f2ce0b121e979d90431494be06cfcf491249fa17
njardarson <arnarn23@ru.is>
2024-08-29 22:33:57 +0000

    Question1_solved
    Easy homework, hope that question two is harder.

--- 01_sequential_estimation/template.py
+++ 01_sequential_estimation/template.py
@@ -1,54 +1,43 @@
-# Author: 
+import numpy as np
+from tools import scatter_2d_data, bar_per_axis
+
+# Author: Arnar Ingi Njardarson
 # Date:
 # Project: 
 # Acknowledgements: 
-#
 
 import matplotlib.pyplot as plt
-import numpy as np
-
-from tools import scatter_2d_data, bar_per_axis
 
 
-def gen_data(
-    n: int,
-    k: int,
-    mean: np.ndarray,
-    std: np.float64
-) -> np.ndarray:
+
+def gen_data(n: int, k: int, mean: np.ndarray, std: np.float64) -> np.ndarray:
     '''Generate n values samples from the k-variate
     normal distribution
     '''
-    pass
+    cov = np.eye(k) * std ** 2  # Covariance matrix
+    return np.random.multivariate_normal(mean, cov, n)
 
 
-def update_sequence_mean(
-    mu: np.ndarray,
-    x: np.ndarray,
-    n: int
-) -> np.ndarray:
+def update_sequence_mean(mu: np.ndarray, x: np.ndarray, n: int) -> np.ndarray:
     '''Performs the mean sequence estimation update
     '''
-    pass
+    return mu + (x - mu) / n
 
 
 def _plot_sequence_estimate():
-    data = None # Set this as the data
-    estimates = [np.array([0, 0])]
+    data = gen_data(200, 3, np.array([1, 2, 3]), 0.5)  # Example data
+    estimates = [np.array([1, 2, 3])]
     for i in range(data.shape[0]):
-        """
-            your code here
-        """
+        estimates.append(update_sequence_mean(estimates[-1], data[i], i + 1))
     plt.plot([e[0] for e in estimates], label='First dimension')
-    """
-        your code here
-    """
+    plt.plot([e[1] for e in estimates], label='Second dimension')
+    plt.plot([e[2] for e in estimates], label='Third dimension')
     plt.legend(loc='upper center')
     plt.show()
 
 
 def _square_error(y, y_hat):
-    pass
+    return (y - y_hat) ** 2
 
 
 def _plot_mean_square_error():
@@ -57,15 +46,12 @@ def _plot_mean_square_error():
 
 # Naive solution to the independent question.
 
-def gen_changing_data(
-    n: int,
-    k: int,
-    start_mean: np.ndarray,
-    end_mean: np.ndarray,
-    std: np.float64
-) -> np.ndarray:
-    # remove this if you don't go for the independent section
-    pass
+def gen_changing_data(n: int, k: int, start_mean: np.ndarray, end_mean: np.ndarray, std: np.float64) -> np.ndarray:
+    data = []
+    for i in range(n):
+        current_mean = start_mean + (end_mean - start_mean) * i / n - 1
+        data.append(gen_data(1, k, current_mean, std))
+    return np.vstack(data)
 
 
 def _plot_changing_sequence_estimate():
@@ -74,7 +60,17 @@ def _plot_changing_sequence_estimate():
 
 
 if __name__ == "__main__":
-    """
-    Keep all your test code here or in another file.
-    """
-    pass
+    # Example 1: Small dataset
+    np.random.seed(1234)
+    example1 = gen_data(2, 3, np.array([0, 1, -1]), 1.3)
+    print("Example 1 output:")
+    print(example1)
+    print("\n")
+
+    # Example 2: Larger dataset
+    np.random.seed(1234)
+    example2 = gen_data(5, 1, np.array([0.5]), 0.5)
+    print("Example 2 output:")
+    print(example2)
+    print("\n")
+    _plot_sequence_estimate()
